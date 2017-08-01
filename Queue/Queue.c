9449942b0e560cc96da58649c1de414c5b999ca5
Nikhil Patil <patilnikhils19@gmail.com>
2017-08-01 09:51:24 -0600

    enque dequeue methods

---
+++ Queue/Queue.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+# define Max 15
+
+int queue[Max];
+int front = 0;
+int rear = -1;
+
+int isEmpty(){
+
+}
+
+int isFull(){
+	
+
+}
+
+void enqueue(int data){
+	if(isFull == 1){
+		printf("Queue is full\n");
+	}
+	else{
+		queue[front] = data;
+		rear = rear+1;
+	}
+}
+
+int dequeue(){
+	if(isEmpty == 1){
+		printf("Empty Queue \n");
+		return -1;
+	}
+	else{
+		int d = queue[front];
+		front = front+1;
+		return d;
+	}
+
+}
+
+void main(int argc, char* args[]){
+	isEmpty();
+	enqueue(1);
+	enqueue(2);
+	enqueue(3);
+	enqueue(4);
+	enqueue(5);
+	printf("data is : %d \n", dequeue());
+
+}
